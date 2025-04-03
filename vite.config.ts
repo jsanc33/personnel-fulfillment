bc46c5ece6abba412a5d110bc81331e568a7d4c2
Joseph Sanchez <sanchezjoseph33@gmail.com>
2025-04-03 14:54:36 -0500

    changing port

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3000, // You can change this to any port you prefer
+    open: true, // Automatically open the app in the browser
+  },
 });
