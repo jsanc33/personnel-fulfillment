5674c20f090f648e268015eb31c3bbab7e7a4b78
Joseph Sanchez <sanchezjoseph33@gmail.com>
2025-04-03 11:02:35 -0500

    created candidate interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,11 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    login: string;
+    avatar_url: string;
+    html_url: string;
+    name?: string;
+    company?: string;
+    location?: string;
+    email?: string;
+    bio?: string;
+};
